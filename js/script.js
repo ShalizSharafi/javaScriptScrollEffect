42056312c91f06fd2bd9acd4ba99057a80f31332
shalizsharafi <shalizoputh@gmail.com>
2026-08-10 08:40:06 -0700

    mouseleave and mouseenter effect on the squares

--- js/script.js
+++ js/script.js
@@ -11,23 +11,41 @@ const square = document.querySelectorAll('.square')
  square.forEach((val,i)=>{
        val.style.width = randWidth(200,300) +'px'
        val.style.height = randWidth(200,350) + 'px'
+       rotateSquare(val,i)
 
-       if(i %2){
-              //odd squares
-              val.style.transform='rotate(4deg)'
-       }else if(i % 4 == 2 ){
-              //even squares
-              val.style.transform='rotate(-6deg)'
-       }else if(i %7 == 3){
-              val.style.transform='rotate(8deg)'
-       }else if(i % 4 == 3 ){
-              val.style.transform='rotate(3deg)'
-       }else{
-              val.style.transform='rotate(-4deg)'
-       }
+       val.addEventListener('mouseenter',(e)=>{
+              val.addEventListener('mousemove',removeRotate)
+       })
+
+       val.addEventListener('mouseleave',(e)=>{
+              val.removeEventListener('mousemove',removeRotate)
+              rotateSquare(val,i)
+       })
+
  })
+
 
  function randWidth(min,max){
        return (Math.floor(Math.random()*(max - min))) + min
  }
 
+function rotateSquare(sq,index){
+       if(index %2){
+              //odd squares
+              sq.style.transform='rotate(4deg)'
+       }else if(index % 4 == 2 ){
+              //even squares
+              sq.style.transform='rotate(-6deg)'
+       }else if(index %7 == 3){
+              sq.style.transform='rotate(8deg)'
+       }else if(index % 4 == 3 ){
+              sq.style.transform='rotate(3deg)'
+       }else{
+              sq.style.transform='rotate(-4deg)'
+       }
+}
+
+
+function removeRotate(e){
+       e.target.style.transform='rotate(0deg)'
+}
